21f78471782363b0adba5bb5ae5fd6f306002869
ROBERTO D <robindabas@hotmail.com>
2025-06-02 18:55:35 -0400

    brackets notation practice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Objects practice
+//Dot notaion practice
 
 let storeManager = {}
 storeManager.health = 30;
@@ -7,7 +7,7 @@ storeManager.emotionalState = 70;
 storeManager = {health: storeManager.health}
 console.log(storeManager)
 
-//Other way 
+//Object literal - with the assignment operator ":" 
 
 let tribalSheikh = {
   name: "Theeb Al-Gaddafi Al-Enzi",
@@ -20,3 +20,13 @@ let tribalSheikh = {
 
 tribalSheikh = {name: tribalSheikh.name};
 console.log(tribalSheikh);
+
+//Brackets notation
+
+let cityBangkok = {}
+cityBangkok['weather'] = 'Sunny & very humid';
+cityBangkok['population'] = '71.7 million';
+cityBangkok['province'] = 'Krung Thep Mahan Nakhon';
+cityBangkok['expats'] = '300,000'
+cityBangkok = cityBangkok.weather;
+console.log(cityBangkok)
